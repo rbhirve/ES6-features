310e767f6f0d7d8b4889e10c22b1165cae52280b
rbhirve <rbhirve@gmail.com>
2018-08-08 20:44:48 +0530

    arrow-function in ES6
    Signed-off-by: rbhirve <rbhirve@gmail.com>

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+// Arrow function in ES6
+
+// Normal fumction syntax in ES5
+setTimeout(function(){
+    console.log('Yay! I am in ES5')
+}, 3000);
+
+// Arrow function syntax in ES6
+setTimeout( () => {
+    console.log('Yay! I am in ES6')
+}, 3000);
