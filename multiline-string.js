a90e93c3def151563f0dceb73e6a1d8ef4fb1555
rbhirve <rbhirve@gmail.com>
2018-08-08 20:13:49 +0530

    multiline string in ES6
    Signed-off-by: rbhirve <rbhirve@gmail.com>

---
+++ multiline-string.js
@@ -0,0 +1,27 @@
+// Multiline string in ES6
+
+// Multiline string in ES5
+var roadPoem1 = 'Then took the other, as just as fair,\n\t'
+    + 'And having perhaps the better claim\n\t'
+    + 'Because it was grassy and wanted wear,\n\t'
+    + 'Though as for that the passing there\n\t'
+    + 'Had worn them really about the same,\n\t';
+
+console.log(roadPoem1);
+
+var fourAgreements1 = 'You have the right to be you.\n\
+    You can only be you when you do your best.'
+
+console.log(fourAgreements1);
+
+// Multiline string in ES6  
+var roadPoem2 = `Then took the other, as just as fair,
+And having perhaps the better claim
+Because it was grassy and wanted wear,
+Though as for that the passing there
+Had worn them really about the same,`
+console.log(roadPoem2);
+
+var fourAgreements2 = `You have the right to be you.
+You can only be you when you do your best.`
+console.log(fourAgreements2);
